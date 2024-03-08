6ee45d429e32730c82db3f9b9041a07e2414fa15
Arpita.s <arpita.sahoo637174@gail.com>
2024-03-08 20:23:27 +0530

    basic method implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,24 @@ import { DataService } from './data.service';
 export class AppComponent {
   title = 'RandomJokes';
   constructor(private dataSvc:DataService){}
+
+  IsLoadingStarted:boolean = false;
+  IsGameStarted:boolean = false;
+  data:any;
   async fetchdata(){
     let fetchedData = await this.dataSvc.fetchJokes();
-    console.log(fetchedData);
+    if(fetchedData){
+      this.data = fetchedData;
+    }
+    console.log(this.data);
     
   }
+
+  StartLoading(){
+    this.IsLoadingStarted = true;
+
+    this.fetchdata();
+    this.IsGameStarted = true;
+  }
 }
+// data is getting read before fetching the data from api
